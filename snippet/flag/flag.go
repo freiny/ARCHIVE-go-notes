a35eec99d3c46f45e25611e4659ad07cc187d246
name <freiny50@gmail.com>
2016-05-29 21:59:33 -0500

    Add snippet/flag/flag.go

---
+++ snippet/flag/flag.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	var name = flag.String("name", "Pat", "help message for name")
+	var age = flag.Int("age", 30, "help message for age")
+
+	fmt.Println("name", *name, "age", *age)
+	flag.Parse()
+	fmt.Println("name", *name, "age", *age)
+
+	// INPUT:
+	// $ command -age 22
+	// OUTPUT:
+	// name Pat age 30
+	// name Pat age 22
+
+	// INPUT:
+	// $ command -name Rita -age 22
+	// OUTPUT:
+	// name Pat age 30
+	// name Rita age 22
+}
